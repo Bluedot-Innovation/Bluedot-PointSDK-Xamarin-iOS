8b78ef5e15dd884e0d9d8107cbdc97316d6df77d
Pavel Oborin <pavel@bluedot.io>
2018-09-20 17:16:11 +1000

    refactor: run sharpie to generate api bindings

---
+++ PointSDK.iOS/Structs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using PointSDK;
+using Foundation;
+using ObjCRuntime;
+
+namespace PointSDK.iOS
+{
+    [Native]
+    public enum BDAuthenticationState : long
+    {
+        NotAuthenticated,
+        Authenticating,
+        Authenticated
+    }
+
+    static class CFunctions
+    {
+        // extern NSString * BDStringFromAuthenticationState (BDAuthenticationState state);
+        [DllImport ("__Internal")]
+        static extern NSString BDStringFromAuthenticationState (BDAuthenticationState state);
+
+        // extern BDLocationCoordinate2D BDLocationCoordinate2DMake (BDLocationDegrees latitude, BDLocationDegrees longitude);
+        [DllImport ("__Internal")]
+        static extern BDLocationCoordinate2D BDLocationCoordinate2DMake (double latitude, double longitude);
+    }
+
+    [StructLayout (LayoutKind.Sequential)]
+    public struct BDLocationCoordinate2D
+    {
+        public double latitude;
+
+        public double longitude;
+    }
+}
