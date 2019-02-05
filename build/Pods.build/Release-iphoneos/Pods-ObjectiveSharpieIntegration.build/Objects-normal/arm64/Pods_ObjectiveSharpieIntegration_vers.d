1d0fb7aff10468ce6a2ddd1663a3beb7ccc032a3
Pavel Oborin <pavel@bluedot.io>
2019-02-06 09:12:00 +1100

    Release/1.13.1

---
+++ build/Pods.build/Release-iphoneos/Pods-ObjectiveSharpieIntegration.build/Objects-normal/arm64/Pods_ObjectiveSharpieIntegration_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/PavelOborin/projects/xamarin-dll-generation/build/Pods.build/Release-iphoneos/Pods-ObjectiveSharpieIntegration.build/DerivedSources/Pods_ObjectiveSharpieIntegration_vers.c
