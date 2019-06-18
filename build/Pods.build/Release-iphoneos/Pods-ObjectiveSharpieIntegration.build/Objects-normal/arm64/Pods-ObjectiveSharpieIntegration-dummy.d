580d4e2263c351fa92a093578ce09730376f2dc6
Pavel Oborin <pavel@bluedot.io>
2019-06-18 15:25:33 +1000

    untrack build

--- build/Pods.build/Release-iphoneos/Pods-ObjectiveSharpieIntegration.build/Objects-normal/arm64/Pods-ObjectiveSharpieIntegration-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/PavelOborin/projects/xamarin-dll-generation/Pods/Target\ Support\ Files/Pods-ObjectiveSharpieIntegration/Pods-ObjectiveSharpieIntegration-dummy.m \
-  /Users/PavelOborin/projects/xamarin-dll-generation/build/Pods.build/Release-iphoneos/Pods-ObjectiveSharpieIntegration.build/module.modulemap
--- build/Pods.build/Release-iphoneos/Pods-ObjectiveSharpieIntegration.build/Objects-normal/arm64/Pods_ObjectiveSharpieIntegration_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/PavelOborin/projects/xamarin-dll-generation/build/Pods.build/Release-iphoneos/Pods-ObjectiveSharpieIntegration.build/DerivedSources/Pods_ObjectiveSharpieIntegration_vers.c
